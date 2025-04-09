0212e890effc5d7b1dd414ca9d0ceeca8871dd94
behalnihal <nihalgupta140@gmail.com>
2025-04-09 11:35:22 +0530

    fix: type error

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,9 +4,9 @@ export default function About() {
   return (
     <Reveal>
       <div className="flex h-25 items-center px-4 container mx-auto font-light dark:text-neutral-300">
-        I'm Nihal, an engineering student from India, passionate about tackling
-        complex problems using data structures and algorithms. I enjoy building
-        projects and exploring cutting-edge technologies to stay at the
+        I&apos;m Nihal, an engineering student from India, passionate about
+        tackling complex problems using data structures and algorithms. I enjoy
+        building projects and exploring cutting-edge technologies to stay at the
         forefront of innovation.
       </div>
     </Reveal>
